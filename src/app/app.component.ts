809d6a3ef08ebdc27a6802dfb847443fae1ce873
Eswar <mynameieswar@gmail.com>
2024-04-30 20:43:58 +0530

    Implemented the IF condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,57 +19,37 @@ export interface IStudent{
 })
 export class AppComponent {
   title = 'shopping-cart';
-  sNo: string | number | boolean | undefined | null = 10;
 
-  totalMarks!: Number;
+  x = 10;
+  y = 20;
 
-  // studentMarks = [10,'20',false, undefined,null];
+  //boxVisibalityStatus = false;
+  // 0 - false
+  // 1 - true
 
-  studentMarks: Array<number | string | Array<number>> = [10,20,30,'40',[10,20]];
-
-  student: IStudent = {
-    name: 'Raju',
-    address: 'Hyd',
-    mobile: "+91-2343234569",
-    totalMarks: 100,
-    isLeader: false
-  }
-
-  student1: IStudent = {
-    name: 'Raju',
-    address: 'Hyd',
-    mobile: "+91-2343234569",
-    totalMarks: 100
-  }
-
-  studentList: Array<IStudent>  =[];
-
-
-
-  additionaInformation: any;
-
-
-
+  boxVisibalityStatus = 0;
+  // 0 - Hide Box
+  // 1 - Show Box
+  // 2 - Show the Message
   constructor(){
-this.additionaInformation = 10;
-this.additionaInformation = false;
-this.additionaInformation = 'Raju';
-this.additionaInformation = null;
-this.additionaInformation = undefined;
-this.additionaInformation =[];
-this.additionaInformation ={}
-
-
-
-
-  console.log(this.title)
-  console.log(this.totalMarks)
-  this.sNo = '10';
-  this.sNo = false;
-  this.sNo = undefined;
-  this.sNo = null;
-  console.log(this.sNo)
-
   }
 
+
+
+  // showBox(){
+  //   console.log('Show Box');
+  //   this.boxVisibalityStatus = true;
+  // }
+
+  // hideBox(){
+  //   console.log('Hide Box');
+  //   this.boxVisibalityStatus = false;
+  // }
+
+  // toggleBox(){
+  //   this.boxVisibalityStatus = !this.boxVisibalityStatus;
+  // }
+  
+
+  
 }
